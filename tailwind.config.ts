4950c1d66cf3124d9ee377054c73f731658c2a69
Gabot3ck <jesus.castilloc@outlook.com>
2024-08-24 01:49:35 -0400

    setting colors in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,9 +24,27 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        // primary: {
+        //   DEFAULT: "hsl(var(--primary))",
+        //   foreground: "hsl(var(--primary-foreground))",
+        // },
+        'primary': {
+            300: '#91b4e8',
+            400: '#2978c4',
+            500: '#1877f2',
+            600: '#044AA5',
+            700: '#022D65',
+        },
+        'dark': {
+          400:'#141c3a',
+          500:'#111827',
+        },
+        'light': {
+          500: '#f5f5f5',
+          600: '#6b7280',
+        },
+        'alert': {
+          500: '#ef4444',
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
